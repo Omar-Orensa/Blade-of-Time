10ab5587dcc3d58f72dc5e0b42a6b6745cb12957
Omar Orensa <omar.orensa319@gmail.com>
2025-09-27 14:02:05 +0300

    Blade of Time prototype

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 6f;
+    public float jumpForce = 10f;
+    public Transform groundCheck;      // empty child at feet
+    public float groundRadius = 0.15f;
+    public LayerMask groundLayer;
+
+    Rigidbody2D rb;
+    bool grounded;
+    float move;
+
+    float baseScaleX;   // <- NEW
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        baseScaleX = Mathf.Abs(transform.localScale.x);   // <- store starting width
+    }
+
+    void Update()
+    {
+        move = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded)
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+        if (move != 0)
+        {
+            var s = transform.localScale;
+            s.x = baseScaleX * (move < 0 ? -1 : 1);       // <- keep size, flip only direction
+            transform.localScale = s;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(move * moveSpeed, rb.linearVelocity.y);
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+    }
+}
